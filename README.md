b8c5a28a31b11fcebeccd889b2a3af9791d39cf2
CORP\himjoshi <himanshu.a.joshi@capgemini.com>
2023-08-24 20:03:44 +0200

    Added solution for assignment 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Angular-Coursera-Assignment2
+# Module 2 assignment solution
+This folder contains the solution for the module 2 assignment
---
+++ app.js
@@ -0,0 +1,60 @@
+(function () {
+    'use strict';
+    angular.module('ShoppingListApp', [])
+        .controller('ShoppingListController', ShoppingListController)
+        .controller('ShoppingListController2', ShoppingListController2)
+        .provider('ShoppingListService', ShoppingListServiceProvider);
+
+    function ShoppingListService(maxItems) {
+        let service = this;
+        let items = [
+            { name: "cookies", quantity: 10 },
+            { name: "coffee pack", quantity: 1 },
+            { name: "milk pack", quantity: 1 },
+            { name: "chicken pack", quantity: 1 },
+            { name: "chocolate pack", quantity: 1 }
+        ];
+        let boughtItems = [];
+
+        service.removeItem = function (itemIndex) {
+            boughtItems.push({ name: items[itemIndex].name, quantity: items[itemIndex].quantity });
+            items.splice(itemIndex, 1);
+        };
+
+        service.getItems = function () {
+            return items;
+        };
+
+        service.getBoughtItems = function () {
+            return boughtItems;
+        };
+    };
+
+    function ShoppingListServiceProvider() {
+        let provider = this;
+        provider.defaults = {
+            maxItems: 10
+        };
+
+        provider.$get = function () {
+            let shoppingList = new ShoppingListService(provider.defaults.maxItems);
+            return shoppingList;
+        };
+    };
+
+    ShoppingListController.$inject = ['ShoppingListService'];
+    function ShoppingListController(ShoppingListService) {
+        let list1 = this;
+        list1.items = ShoppingListService.getItems();
+
+        list1.removeItem = function (itemIndex) {
+            ShoppingListService.removeItem(itemIndex);
+        };
+    };
+
+    ShoppingListController2.$inject = ['ShoppingListService'];
+    function ShoppingListController2(ShoppingListService) {
+        let list2 = this;
+        list2.items = ShoppingListService.getBoughtItems();
+    };
+})();
